1564209bf48a2d8395bdec4f6610ba52e7040353
pengxinglove <pengxinglove@qq.com>
2017-06-12 23:12:54 +0800

    init

---
+++ DianDuInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parseTextByTab
+{
+    public class DianDuInfo
+    {
+        public String bookName { set; get; }
+        public String imagePath { set; get; }
+        public String imageContentType { set; get; }
+        public String bookPage { set; get; }
+        public String BookInfo { set; get; }
+
+        public String ISBN { set; get; }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace parseTextByTab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String path = @"F:\pengxing\人教小学语文1年级下册(家教机)(6.3).drm\人教小学语文1年级下册(家教机)(6.3).drm.xls";
+            parseText(path);
+        }
+
+        private void parseText(String path)
+        {
+            List<DianDuInfo> dianDuInfos = new List<DianDuInfo>();
+            DianDuInfo temp = new DianDuInfo();
+            String fileName = Path.GetFileNameWithoutExtension(path);
+            Console.WriteLine("fileName " + fileName );
+            bool isFirstRow = true;
+            foreach (var line in File.ReadLines(path).Skip(1))
+            {
+                var tempLine = line.Split('\t');
+                temp.bookName = fileName;
+                //if()
+                temp.imagePath = tempLine[1];
+                if (temp.imagePath.EndsWith(".jpg") && isFirstRow)
+                {
+                    temp.imagePath = "pic\\" + fileName + ".jpg";
+                    isFirstRow = false;
+                }
+                temp.imageContentType = tempLine[2];
+
+                temp.bookPage = tempLine[3];
+                temp.BookInfo = tempLine[4];
+                temp.ISBN = tempLine[5];
+
+                Console.WriteLine("imagePath " + temp.imagePath + " - page " + temp.bookPage +
+                    " -ISBN " + temp.ISBN);
+                dianDuInfos.Add(temp);
+            }
+        }
+    }
+}
